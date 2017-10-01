5ec2c6c9d9b2723df6a1272342bc5a065e7db27d
Tnarita <naritatakuya0000@gmail.com>
2017-10-02 05:58:55 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <h3>{{$store.state.counter}}</h3>
+    <button @click='increment'></button><br>
+    <a href="/login">Login</a>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  created: function() {
+  },
+  methods: {
+    increment: function() {
+      this.$store.commit('increment');
+    },
+    signIn: function() {
+      this.$store.commit('signIn');
+    }
+  }
+}
+</script>
