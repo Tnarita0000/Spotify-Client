b4b4177235223ed15a814f2f4150ffbd9c64315e
Tnarita0000 <naritatakuya0000@gmail.com>
2017-10-06 01:20:47 +0900

    clean & update

--- pages/login.vue
+++
@@ -1,5 +0,0 @@
-<template>
-</template>
-<script>
-import axios from 'axios'
-</script>
---
+++ src/client/viewmodels/home.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import AppComponent from '../components/App.vue'
+
+new Vue({
+  el: "#app",
+  components: {
+    AppComponent
+  }
+});
